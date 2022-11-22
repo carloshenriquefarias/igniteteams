56f466b389f34e665e735fdd17d99f85082edb91
Carlos Henrique <you@example.comcarloshenriquepvh@hotmail.com>
2022-11-22 16:25:31 -0400

    Estilização dos componentes

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Groups } from './src/screens/Groups';
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+// import { Groups } from './src/screens/Groups';
+import { Groups } from '@screens/Groups';
+
 
 export default function App() {
   return (
