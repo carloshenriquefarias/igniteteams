2e064c4c3938464fc31dda639772a1fb08b2a132
Carlos Henrique <you@example.comcarloshenriquepvh@hotmail.com>
2022-11-27 20:30:53 -0400

    Criação de novos componentes

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,20 @@
+import {Container, Title, ButtonTypeStyleProps  } from './styles';
+import { TouchableOpacityProps } from "react-native";
+
+type Props = TouchableOpacityProps & {
+    title: string;
+    type?: ButtonTypeStyleProps;
+}
+
+export function Button({title, type = 'PRIMARY', ...rest}: Props ){
+    return(
+        <Container 
+            type={type} 
+            {...rest}
+        >        
+            <Title>
+                {Title}
+            </Title>
+        </Container>
+    );
+}
--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,12 +3,13 @@ import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 import {Container } from './styles';
 import {useState} from 'react'
 
 export function Groups() {
 
-  const [groups, setGroups] = useState<string[]>([])
+  const [groups, setGroups] = useState<string[]>(['Os Bons', 'Familia', 'Amigos'])
 
   return (
     <Container>
@@ -30,6 +31,11 @@ export function Groups() {
         contentContainerStyle={groups.length === 0 && {flex: 1}}
         ListEmptyComponent={() => <ListEmpty message='Que tal cadastrar a primeira turma?'/>}
       />
+
+      {/* <Button
+        title='Criar uma nova turma'
+        // onPress={}
+      /> */}
 
       {/* <GroupCard title="Galera dos BONS" /> */}
     </Container>
